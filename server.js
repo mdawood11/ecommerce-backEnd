9af38dd05563d8912d404db2e46e267d26717089
mohamad ghaznawy <mdawoodghaznawy@yahoo.com.com>
2023-04-10 23:37:31 -0700

    Added my folder develop and fixed bugs caused running the app

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const routes = require("./routes");
+const routes = require("./Develop/routes");
+const sequelize = require("./Develop/config/connection");
 // import sequelize connection
-const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
+// sync sequelize models to the database, then turn on the server.js
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
