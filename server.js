b7f5a55f84cacfee221cffa73cf9cefa7eb97225
mohamad ghaznawy <mdawoodghaznawy@yahoo.com.com>
2023-04-12 21:25:41 -0700

    fixed bug

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const routes = require("./Develop/routes");
-const sequelize = require("./Develop/config/connection");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
 // import sequelize connection
 
 const app = express();
